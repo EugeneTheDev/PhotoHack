2841ed705b4c37df65c88218c48b1d2e17b80471
Eugene <eugene.seager@gmail.com>
2019-03-16 12:10:40 +0300

    Add upload route

--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
+import flask
+from flask import request
 from flask import Flask
 
 app = Flask(__name__)
+base_url = "/api"
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route(f"{base_url}/upload", methods=["POST"])
+def upload_info():
+    request.files["upload"].save("./static/image.png")
+    print(request.form["text"])
+    return flask.jsonify({
+        "message": "Successfully uploaded"
+    })
 
 
-if __name__ == '__main__':
-    app.run()
