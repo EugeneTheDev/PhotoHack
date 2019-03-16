4c84366ed7d61adaa2e1589431e72b19fcf5dca4
KirillIvano <ivannadez@yandex.ru>
2019-03-16 12:24:14 +0300

    added streaming

--- src/app.py
+++ src/app.py
@@ -2,11 +2,13 @@ from flask import Flask, request
 import requests
 app = Flask(__name__)
 
+def textAnalyze():
+
+
 
 @app.route('/stream_file', methods=["POST"])
 def stream_file():
     file = request.files['file']
-    sendFile = {"file": (file.filename, file.stream, file.)}
+    sendFile = {"file": (file.filename, file.stream, file.mimetype)}
+    r = requests.post("http://myservicedotcom/upload", files=sendFile)
 
-
-app.run()
